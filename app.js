c910514afedef1eca46f9a0ceeb4707cf15c19f0
Shayan Javadi <shayanjavadi1375@gmail.com>
2017-05-09 02:32:22 -0500

    added event listener to make navbar small when scrolling down

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-//
-//
-// slider.classList.remove("section-hero-1");
-// slider.classList.add("section-hero-2");
+//waypoints
+
+let lastScrollTop = 0;
+
+class NavControl {
+  constructor() {
+    this.headerTop = document.querySelector(".header-top--js");
+  }
+
+  detectScroll() {
+    let _this = this;
+    window.addEventListener("scroll", function(){
+       let st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+       if (st > lastScrollTop){
+         _this.headerTop.classList.add("header-top-mini");
+       } else {
+         _this.headerTop.classList.remove("header-top-mini");
+       }
+       lastScrollTop = st;
+    }, false);
+  }
+}
+
+navTest = new NavControl;
+navTest.detectScroll();
+// slider
+
 class Slider {
   constructor(slideTime) {
     let slider = document.querySelector(".slider--js");
@@ -17,7 +40,6 @@ class Slider {
 
       //remove current slide
       _this.slider.classList.remove("section-hero-" + _this.currentSlide);
-
       //reset back to 1 since we only have a certain amount of slides
       // TODO: make number of slides dynamic
 
@@ -26,12 +48,21 @@ class Slider {
       }
       //iterate once
       _this.currentSlide++;
+
+      //re-trigger css animation
+      _this.slider.style.webkitAnimation = 'none';
+        setTimeout(function() {
+        _this.slider.style.webkitAnimation = '';
+        _this.slider.classList.remove("loading");
+      }, 10);
+
       //add the next slide
       _this.slider.classList.add("section-hero-" + _this.currentSlide);
-
+      //hide div at first so it doens't load twice
+      _this.slider.classList.add("loading");
     }, _this.slideTime);
   }
 }
 
-slider = new Slider(2000);
+slider = new Slider(10000);
 slider.start();
